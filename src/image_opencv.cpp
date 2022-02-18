3830183c4468e56ef14e427a86db719d15d19015
reliverexision <kaimeng997@gmail.com>
2022-02-18 12:33:16 +0800

    Updated Configurations

--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -9,6 +9,56 @@ using namespace cv;
 
 extern "C" {
 
+// Added, solution from https://github.com/pjreddie/darknet/issues/1886
+Mat image_to_mat(image im)
+{
+image copy = copy_image(im);
+constrain_image(copy);
+if(im.c == 3) rgbgr_image(copy);
+
+Mat m(cv::Size(im.w,im.h), CV_8UC(im.c));
+int x,y,c;
+
+int step = m.step;
+for(y = 0; y < im.h; ++y){
+    for(x = 0; x < im.w; ++x){
+        for(c= 0; c < im.c; ++c){
+            float val = im.data[c*im.h*im.w + y*im.w + x];
+            m.data[y*step + x*im.c + c] = (unsigned char)(val*255);
+        }
+    }
+}
+
+free_image(copy);
+return m;
+
+}
+
+image mat_to_image(Mat m)
+{
+
+int h = m.rows;
+int w = m.cols;
+int c = m.channels();
+image im = make_image(w, h, c);
+unsigned char *data = (unsigned char *)m.data;
+int step = m.step;
+int i, j, k;
+
+for(i = 0; i < h; ++i){
+    for(k= 0; k < c; ++k){
+        for(j = 0; j < w; ++j){
+            im.data[k*w*h + i*w + j] = data[i*step + j*c + k]/255.;
+        }
+    }
+}
+rgbgr_image(im);
+return im;
+
+}
+// End of add
+
+/*
 IplImage *image_to_ipl(image im)
 {
     int x,y,c;
@@ -65,6 +115,7 @@ image mat_to_image(Mat m)
     rgbgr_image(im);
     return im;
 }
+*/
 
 void *open_video_stream(const char *f, int c, int w, int h, int fps)
 {
@@ -72,9 +123,14 @@ void *open_video_stream(const char *f, int c, int w, int h, int fps)
     if(f) cap = new VideoCapture(f);
     else cap = new VideoCapture(c);
     if(!cap->isOpened()) return 0;
+    /* Changed
     if(w) cap->set(CV_CAP_PROP_FRAME_WIDTH, w);
     if(h) cap->set(CV_CAP_PROP_FRAME_HEIGHT, w);
     if(fps) cap->set(CV_CAP_PROP_FPS, w);
+    */
+    if(w) cap->set(CAP_PROP_FRAME_WIDTH, w);
+    if(h) cap->set(CAP_PROP_FRAME_HEIGHT, w);
+    if(fps) cap->set(CAP_PROP_FPS, w);
     return (void *) cap;
 }
 
@@ -123,7 +179,10 @@ void make_window(char *name, int w, int h, int fullscreen)
 {
     namedWindow(name, WINDOW_NORMAL); 
     if (fullscreen) {
+        /* Changed
         setWindowProperty(name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+        */
+        setWindowProperty(name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
     } else {
         resizeWindow(name, w, h);
         if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
